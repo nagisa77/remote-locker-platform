e3c94ef78ddd39a8b22b945f5d2dc39ed69ad5f7
tim <nagisa12164@163.com>
2024-10-16 19:37:25 +0800

    feat: 更新public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/<repository-name>/' // 替换为你的仓库名
+  : '/',
   transpileDependencies: true,
   configureWebpack: {
     devtool: 'source-map'
